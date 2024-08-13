451b40f183634493eaa42b16e4a36cb58ddbf156
Javier Mérida <javiermerida202230638@cunoc.edu.gt>
2024-08-13 01:33:48 -0600

    Primera carga de los archivos de Netbeans

---
+++ PrimeraPracticaIPC2/src/main/java/primerapracticaipc2/primerapracticaipc2/PrimeraPracticaIPC2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package primerapracticaipc2.primerapracticaipc2;
+
+/**
+ *
+ * @author xavi
+ */
+public class PrimeraPracticaIPC2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
