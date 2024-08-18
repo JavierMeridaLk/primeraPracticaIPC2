c41d40c2f015d88a3ecba51d9f2071758a0272d3
Javier Mérida <javiermerida202230638@cunoc.edu.gt>
2024-08-18 15:38:21 -0600

    Verificacion de estado de tarjeta

--- PrimeraPracticaIPC2/src/main/java/Backend/Movimientos.java
+++ PrimeraPracticaIPC2/src/main/java/Backend/Movimientos.java
@@ -28,59 +28,68 @@ public class Movimientos {
         
     }
     
-    public void realizarMovimiento(String numTarjeta, LocalDate fecha, String tipo, String descripcion, String establecimiento, double monto) {
+    public void realizarMovimiento(String numTarjeta, LocalDate fecha, String tipo, String descripcion, String establecimiento, double monto) {      
         double saldo=0;
         double total=0;
-        
+        boolean estadoTarjeta=false;
+                
         String insert = "INSERT INTO movimientos (num_tarjeta, fecha_movimiento, tipo_movimiento, descripcion, codigo_establecimiento, monto)"
                 + "values('"+ numTarjeta + "','"+ fecha + "','"+ tipo + "','"+ descripcion + "','"+ establecimiento + "','"+ monto + "')";
         
         try{
-            Statement statementInsert = gestor.getConnection().createStatement();
-            int rowsAffected = statementInsert.executeUpdate(insert);
-            
-            System.out.println("Rows affected" + rowsAffected);
             
             try {
                 String select = "SELECT * FROM tarjeta WHERE numero_tarjeta = ?";
-            PreparedStatement statement = gestor.getConnection().prepareStatement(select);
-            statement.setString(1, numTarjeta);
-            ResultSet resultSet = statement.executeQuery();
+                PreparedStatement statement = gestor.getConnection().prepareStatement(select);
+                statement.setString(1, numTarjeta);
+                ResultSet resultSet = statement.executeQuery();
 
-            while(resultSet.next()){
-                 saldo =resultSet.getDouble("saldo_tarjeta");
-            }
-                } catch (Exception e) {
+                while(resultSet.next()){
+                    saldo =resultSet.getDouble("saldo_tarjeta");
+                    estadoTarjeta = resultSet.getBoolean("estado_tarjeta");
+                }
+            } catch (Exception e) {
+                
             }
             
-            
-            String update = "UPDATE tarjeta SET saldo_tarjeta = ? WHERE numero_tarjeta = ?";
-                        PreparedStatement updateStatement = gestor.getConnection().prepareStatement(update);
-                        if (tipo.equalsIgnoreCase("cargo")) {
-                total= saldo-monto;
+            if (estadoTarjeta) {
+                Statement statementInsert = gestor.getConnection().createStatement();
+                int rowsAffected = statementInsert.executeUpdate(insert);
+                System.out.println("Rows affected" + rowsAffected);
+                String update = "UPDATE tarjeta SET saldo_tarjeta = ? WHERE numero_tarjeta = ?";
+                PreparedStatement updateStatement = gestor.getConnection().prepareStatement(update);
+             
+                if (tipo.equalsIgnoreCase("cargo")) {
+                    total= saldo-monto;
+                }else{
+                    total= saldo+monto;
+                }
+                        
+                updateStatement.setBigDecimal(1, BigDecimal.valueOf(total));    
+                updateStatement.setString(2, numTarjeta);
+                int rowsUpdated = updateStatement.executeUpdate();
+                       
+                if (rowsUpdated > 0) {
+                    JOptionPane.showMessageDialog(
+                    null,
+                    "Movimiento exitoso, saldo actualizado.",
+                    "APROBADO",
+                    JOptionPane.INFORMATION_MESSAGE
+                    );
+                }
+                
             }else{
-                            total= saldo+monto;
-                        }
-                        
-                        updateStatement.setBigDecimal(1, BigDecimal.valueOf(total));
-                        
-                        updateStatement.setString(2, numTarjeta);
-                        int rowsUpdated = updateStatement.executeUpdate();
-                        
-                        if (rowsUpdated > 0) {
-                            JOptionPane.showMessageDialog(
-                                null,
-                                "Movimiento exitoso, saldo actualizado.",
-                                "APROBADO",
-                                JOptionPane.INFORMATION_MESSAGE
-                            );
-                        }
+                
+                JOptionPane.showMessageDialog(
+                    null,
+                    "La tarjeta ha sido cancelada.",
+                    "ERROR",
+                    JOptionPane.ERROR_MESSAGE
+                    );
+            }
         }catch(SQLException e){
             System.out.println("Error al conectar a la DB");
             e.printStackTrace();
-        }
-        
-        
-    
-}
+        }   
+    }
 }
